bc25e7e3f4f4b3a16281631b163d994bc76e042b
Achyutashivam <shivam@achyutagroup.com>
2026-03-07 16:44:31 +0530

    wfg

--- scrapers/daily/allindiambacollegedetails181_220.py
+++ scrapers/daily/allindiambacollegedetails181_220.py
@@ -98,7 +98,7 @@ def create_driver():
         "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     )
 
-    service = Service("/snap/bin/chromedriver")
+    service = Service("/usr/bin/chromedriver")
 
     driver = webdriver.Chrome(
         service=service,
